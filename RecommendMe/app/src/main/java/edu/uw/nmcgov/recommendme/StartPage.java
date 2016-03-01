755f989264831348504fbc8018d1220a713568c2
nathaliemcgov <nathalie.mcgovern@gmail.com>
2016-02-29 20:55:48 -0800

    home page and recommendations for you page w/ hard coded tiles

--- RecommendMe/app/src/main/java/edu/uw/nmcgov/recommendme/StartPage.java
+++ RecommendMe/app/src/main/java/edu/uw/nmcgov/recommendme/StartPage.java
@@ -1,13 +1,31 @@
 package edu.uw.nmcgov.recommendme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartPage extends AppCompatActivity {
+
+    private Button exploreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+
+        exploreButton = (Button) findViewById(R.id.exploreButton);
+        exploreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exploreRecommendations();
+            }
+        });
+    }
+
+    private void exploreRecommendations() {
+        Intent intent = new Intent(this, recommendationsForYou.class);
+        startActivity(intent);
     }
 }
---
+++ RecommendMe/app/src/main/java/edu/uw/nmcgov/recommendme/recommendationsForYou.java
@@ -0,0 +1,35 @@
+package edu.uw.nmcgov.recommendme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+public class recommendationsForYou extends AppCompatActivity {
+
+    private GridView tileGrid;
+    private ArrayList<String> recommendationList;
+    ArrayAdapter<String> adapter;
+
+    static final String[] recommendations = new String[] {
+            "The Departed", "The Illiad"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommendations_for_you);
+
+        tileGrid = (GridView) findViewById(R.id.recommendationList);
+
+        recommendationList = new ArrayList<String>();
+        recommendationList.add("");
+
+        adapter = new ArrayAdapter<String>(this,
+                R.layout.recommendation_element, recommendations);
+
+        tileGrid.setAdapter(adapter);
+    }
+}
