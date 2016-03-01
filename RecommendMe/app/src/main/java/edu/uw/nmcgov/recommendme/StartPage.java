96eda7a3da05a42efe65677a4bb08e175844ac75
nathaliemcgov <nathalie.mcgovern@gmail.com>
2016-02-29 23:07:10 -0800

    changed background color + made tiles clickable

--- RecommendMe/app/src/main/java/edu/uw/nmcgov/recommendme/StartPage.java
+++ RecommendMe/app/src/main/java/edu/uw/nmcgov/recommendme/StartPage.java
@@ -1,12 +1,13 @@
 package edu.uw.nmcgov.recommendme;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class StartPage extends AppCompatActivity {
+public class StartPage extends Activity {
 
     private Button exploreButton;
 
--- RecommendMe/app/src/main/java/edu/uw/nmcgov/recommendme/recommendationsForYou.java
+++ RecommendMe/app/src/main/java/edu/uw/nmcgov/recommendme/recommendationsForYou.java
@@ -2,8 +2,11 @@ package edu.uw.nmcgov.recommendme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -31,5 +34,11 @@ public class RecommendationsForYou extends AppCompatActivity {
                 R.layout.recommendation_element, recommendations);
 
         tileGrid.setAdapter(adapter);
+
+        tileGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(RecommendationsForYou.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
