07c33fbbde1cbc1ebd0a12e13e99a9da0906e92b
tyler jensen <tylerj11@uw.edu>
2016-03-08 00:00:52 -0800

    trying tries

--- phpWebService/uploadFiles.php
+++ phpWebService/uploadFiles.php
@@ -1,190 +1,167 @@
 <?php 
 
 
-$trie = new Trie();
+$trie = new KTrie();
 
-$trie->add("key", 5);
-print $trie->search("ke");
+$trie->addWord("cat");
+$trie->addWord("coins");
+print $trie;
 
 
-/*
-Copyright (c) 2009, Francisco Facioni
-All rights reserved.
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * The names of its contributors may not be used to endorse or promote products
-      derived from this software without specific prior written permission.
-THIS SOFTWARE IS PROVIDED BY Francisco Facioni ''AS IS'' AND ANY
-EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL Francisco Facioni BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-/**
- * Class Trie
- *
- * @package PHPTrie
- */
-class Trie
+class KTrie
 {
-    private $trie = array();
-    private $value = null;
-    /**
-     * Trie constructor
-     *
-     * @param mixed $value This is for internal use
-     */
-    public function __construct($value = null)
+    function __construct()
     {
-        $this->value = $value;
+        $this->root = new KTrieNode('');
     }
-    /**
-     * Add value to the trie
-     *
-     * @param string $string    The key
-     * @param mixed  $value     The value
-     * @param bool   $overWrite Overwrite existing value
-     */
-    public function add($string, $value, $overWrite=true)
+    
+    function addWord($word)
     {
-        if ($string === "") {
-            if (is_null($this->value) || $overWrite) {
-                $this->value = $value;
-            }
-            return;
-        }
-        foreach ($this->trie as $prefix => $trie) {
-            $prefix = (string)$prefix;
-            $prefixLength = strlen($prefix);
-            $head = substr($string,0,$prefixLength);
-            $headLength = strlen($head);
-            $equals = true;
-            $equalPrefix = "";
-            for ($i= 0;$i<$prefixLength;++$i) {
-                //Split
-                if ($i >= $headLength) {
-                    $equalTrie = new Trie($value);
-                    $this->trie[$equalPrefix] = $equalTrie;
-                    $equalTrie->trie[substr($prefix,$i)] = $trie;
-                    unset($this->trie[$prefix]);
-                    return;
-                } elseif ($prefix[$i] != $head[$i]) {
-                    if ($i > 0) {
-                        $equalTrie = new Trie();
-                        $this->trie[$equalPrefix] = $equalTrie;
-                        $equalTrie->trie[substr($prefix,$i)] = $trie;
-                        $equalTrie->trie[substr($string,$i)] = new Trie($value);
-                        unset($this->trie[$prefix]);
-                        return;
-                    }
-                    $equals = false;
-                    break;
-                }
-                $equalPrefix .= $head[$i];
-            }
-            if ($equals) {
-                $trie->add(substr($string,$prefixLength),$value,$overWrite);
-                return;
-            }
-        }
-        $this->trie[$string] = new Trie($value);
+        $wordArray = str_split($word);
+        $this->root->addSuffix($wordArray);
     }
-    /**
-     * Search the Trie with a string
-     *
-     * @param $string The string search
-     *
-     * @return mixed The value
-     */
-    public function search($string)
+    
+    function hasWord($word)
     {
-        if (empty($string)) {
-            return $this->value;
-        }
-        foreach ($this->trie as $prefix => $trie) {
-            $prefix = (string)$prefix;
-            $prefixLength = strlen($prefix);
-            $head = substr($string,0,$prefixLength);
-            if ($head === $prefix) {
-                return $trie->search(substr($string,$prefixLength));
+        $wordArray = str_split($word);
+        return $this->root->hasSuffix($wordArray);
+    }
+    
+    function printStructure()
+    {
+        $this->root->printStructure(0);
+    }
+    
+    function printWords()
+    {
+        $this->root->printWords('');
+    }
+
+    function getWordsFromPrefix($prefix) {
+        //navigate to node
+        $node = $this->root;
+        for($i = 1; $i <= strlen($prefix); $i++) {
+            if($node->hasSuffix(substr($prefix, 0, $i))) {
+                $node = $node->getChild($prefix[$i]);
             }
         }
-        return null;
+
+        $node->printWords($prefix);
     }
-    /**
-     * Search with multiple keys
-     *
-     * @param array  $array     The array of keys
-     * @param string $delimeter
-     *
-     * @return mixed The value
-     */
-    public function searchMultiple(array $array, $delimeter=' ')
+
+
+}
+ 
+class KTrieNode
+{
+    function __construct($s){ $this->value = $s; }
+    
+    function addSuffix($suffixArray)
     {
-        $size = count($array);
-        $value = null;
-        for ($j=0;$j<$size;++$j) {
-            $trie = $this;
-            $delim = '';
-            $key = '';
-            for ($i=$j;$i<$size;++$i) {
-                $key .= $delim.$array[$i];
-                $ret = $trie->searchTrie($key);
-                if (is_null($ret)) {
-                    break;
-                }
-                $trie = $ret[1];
-                $key = $ret[0];
-                $delim = $delimeter;
-                if (!is_null($trie->value)) {
-                    $value = $trie->value;
+        if(!empty($suffixArray))
+        {
+            $firstChar = $suffixArray[0];
+            $remnant = array_slice($suffixArray, 1);
+            
+            $childNode = $this->getChild($firstChar);
+            if($childNode === FALSE)
+            {
+                $childNode = $this->addChild($firstChar);
+            }
+            $childNode->addSuffix($remnant);
+        }
+        else
+        {
+            $this->finishesWord = TRUE;
+        }
+    }
+    
+    function hasSuffix($suffixArray)
+    {
+        if(!empty($suffixArray))
+        {
+            $firstChar = $suffixArray[0];
+            
+            $childNode = $this->getChild($firstChar);
+            if($childNode == FALSE)
+            {
+                return FALSE;
+            }   
+            else
+            {
+                $remnant = array_slice($suffixArray, 1);
+                return $childNode->hasSuffix($remnant);
+            }
+        }
+        else 
+        {
+            return TRUE;
+        }
+    }       
+    
+    function getChild($childString)
+    {
+        if(is_array($this->children))
+        {
+            foreach ($this->children as $child)
+            {
+                if($child->value === $childString)
+                {
+                    return $child;
                 }
             }
-            if (!is_null($value)) {
-                return $value;
-            }
         }
-        return null;
+        return FALSE;
     }
-    private function searchTrie($string)
+    
+    function addChild($childString)
     {
-        if (empty($string)) {
-            return array($string,$this);
-        }
-        $stringLength = strlen($string);
-        foreach ($this->trie as $prefix => $trie) {
-            $prefix = (string)$prefix;
-            $prefixLength = strlen($prefix);
-            if ($prefixLength > $stringLength) {
-                $prefix = substr($prefix,0,$stringLength);
-                if ($prefix === $string) {
-                    return array($string,$this);
+        if(is_array($this->children))
+        {
+            foreach($this->children as $child)
+            {
+                if($child->value === $childString)
+                {
+                    return $child;
                 }
             }
-            $head = substr($string,0,$prefixLength);
-            if ($head === $prefix) {
-                return $trie->searchTrie(substr($string,$prefixLength));
+        }
+        
+        $child = new KTrieNode($childString);
+        $this->children[] = $child;
+        
+        return $child;
+    }
+    
+    function printStructure($level)
+    {
+        for($i=0; $i<$level; $i++)
+        {
+            echo ".";
+        }
+        echo $this->value.'<br/>';
+        if(is_array($this->children))
+        {
+            foreach($this->children as $child)
+            {
+                $child->printStructure($level + 1);
             }
         }
-        return null;
     }
-    public static function __set_state($state)
+    
+    function printWords($prefix)
     {
-        $t = new self;
-        $t->trie = $state['trie'];
-        $t->value = $state['value'];
-        return $t;
+        if($this->finishesWord)
+        {
+            echo $prefix.$this->value.'<br/>';
+        }
+        
+        if(is_array($this->children))
+        {
+            foreach($this->children as $child)
+            {
+                $child->printWords($prefix.$this->value);
+            }
+        }
     }
 }
-
- ?>
