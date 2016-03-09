f4dfb8a98a67dd529af2a63475637f69eba31331
Austin Weale <austinweale@gmail.com>
2016-03-09 01:10:47 -0800

    fixed the merge stuff

--- RecommendMe/app/src/main/java/edu/uw/nmcgov/recommendme/AMoneyPage.java
+++ RecommendMe/app/src/main/java/edu/uw/nmcgov/recommendme/AMoneyPage.java
@@ -1,18 +1,19 @@
 package edu.uw.nmcgov.recommendme;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
  * Created by iguest on 3/8/16.
  */
 public class AMoneyPage extends AppCompatActivity {
-    private Button loginBtn;
-    private Button exploreButton;
     private static final String TAG = "start";
     private String[] types;
     private int index;
@@ -20,7 +21,7 @@ public class AMoneyPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_page);
+        setContentView(R.layout.activity_austin_start_page);
         index = 0;
 
         types = new String[5];
@@ -37,30 +38,49 @@ public class AMoneyPage extends AppCompatActivity {
         Button button = (Button)findViewById(R.id.next_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                searchForRecommendations();
+            public void onClick(View V) {
+                if(index < types.length) {
+                    if(index >= 2){
+                        addFragment(types[index]);
+                        EditText editText = (EditText)findViewById(R.id.email_pass);
+                        editText.setVisibility(View.INVISIBLE);
+                        editText.setHeight(0);
+                        TextView text = (TextView)findViewById(R.id.confirm);
+                        text.setVisibility(View.INVISIBLE);
+                        text.setHeight(0);
+                        EditText confirm = (EditText)findViewById(R.id.email_pass_confirm);
+                        confirm.setVisibility(View.INVISIBLE);
+                        confirm.setHeight(0);
+                    }
+                    Log.v(TAG, "changed " + types[index]);
+                    TextView text = (TextView) findViewById(R.id.type_box);
+                    text.setText(types[index]);
+                    index++;
+                }else{
+                    Log.v(TAG, "to next page!");
+                    sendToNext();
+                }
             }
         });
 
-        // Click listener for login button
-        loginBtn = (Button) findViewById(R.id.loginBtn);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                recommendationsForYou();
-            }
-        });
     }
 
-    // Starts activity so user can search title
-    private void searchForRecommendations() {
-        Intent intent = new Intent(this, SearchForRecommendations.class);
+    public void sendToNext(){
+        Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
     }
 
-    // Starts explore recommendations activity
-    private void recommendationsForYou() {
-        Intent intent = new Intent(this, RecommendationsForYou.class);
-        startActivity(intent);
+    public void addFragment(String type){
+
+
+        Bundle bundle = new Bundle();
+        bundle.putString("type", type);
+        Fragment edit = new EditFragment();
+        edit.setArguments(bundle);
+        getFragmentManager().beginTransaction()
+                .replace(R.id.add_box, edit)
+                .commit();
+
     }
 }
+
