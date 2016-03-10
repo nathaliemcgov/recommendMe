f4ffb5ce6087b84e7ec98417d22b3c5b15db683d
Austin Weale <austinweale@gmail.com>
2016-03-09 20:14:09 -0800

    existing user

---
+++ RecommendMe/app/src/main/java/edu/uw/nmcgov/recommendme/ExistingLogin.java
@@ -0,0 +1,63 @@
+package edu.uw.nmcgov.recommendme;
+
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by austinweale on 3/9/16.
+ */
+public class ExistingLogin extends AppCompatActivity {
+
+    private static final String TAG = "existing";
+    private int count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.existing_login_activity);
+        count = 0;
+
+
+        changeVisibility(R.id.existing_password, View.GONE, 0);
+
+
+        Button button = (Button)findViewById(R.id.proceed);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                Log.v(TAG, "back");
+                if(count < 1) {
+                    changeVisibility(R.id.existing_password, View.VISIBLE, ActionBar.LayoutParams.WRAP_CONTENT);
+                    changeVisibility(R.id.existing_email, View.GONE, 0);
+                    count++;
+                }else{
+                    sendToNext();
+                }
+            }
+        });
+
+    }
+
+    public void sendToNext(){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+    public void changeVisibility(int id, int visibility, int height){
+        LinearLayout password = (LinearLayout)findViewById(id);
+        password.setVisibility(visibility);
+        ViewGroup.LayoutParams params = password.getLayoutParams();
+        // Changes the height and width to the specified *pixels*
+        params.height = height;
+    }
+}
