f1a11e8b91f65882bead72fae232b59f55f86ca0
Austin Weale <austinweale@gmail.com>
2016-03-09 22:09:24 -0800

    added some toast bb

--- RecommendMe/app/src/main/java/edu/uw/nmcgov/recommendme/ExistingLogin.java
+++ RecommendMe/app/src/main/java/edu/uw/nmcgov/recommendme/ExistingLogin.java
@@ -2,6 +2,7 @@ package edu.uw.nmcgov.recommendme;
 
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by austinweale on 3/9/16.
@@ -37,15 +39,34 @@ public class ExistingLogin extends AppCompatActivity {
             public void onClick(View V) {
                 Log.v(TAG, "back");
                 if(count < 1) {
-                    changeVisibility(R.id.existing_password, View.VISIBLE, ActionBar.LayoutParams.WRAP_CONTENT);
-                    changeVisibility(R.id.existing_email, View.GONE, 0);
-                    count++;
+                    EditText edit = (EditText)findViewById(R.id.existing_email_entry);
+                    if(edit.getText().length() == 0){
+                        toasted("email");
+                    }else {
+                        changeVisibility(R.id.existing_password, View.VISIBLE, ActionBar.LayoutParams.WRAP_CONTENT);
+                        changeVisibility(R.id.existing_email, View.GONE, 0);
+                        count++;
+                    }
                 }else{
-                    sendToNext();
+                    EditText edit = (EditText)findViewById(R.id.existing_password_entry);
+                    if(edit.getText().length() == 0){
+                        toasted("password");
+                    }else {
+                        sendToNext();
+                    }
                 }
             }
         });
 
+    }
+
+    public void toasted(String type){
+        Context context = getApplicationContext();
+        CharSequence text = "please enter a " + type;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     public void sendToNext(){
