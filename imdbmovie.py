76ffa37bf022066221b263feb51dbed31c4e157b
Tyler Jensen <tylerj11@gmail.com>
2016-05-18 21:17:29 -0700

    movie script

--- imdbmovie.py
+++ imdbmovie.py
@@ -1,17 +1,19 @@
 from bs4 import BeautifulSoup
 import urllib
 import csv
+import re
 
 artistList = []
 
-url = 'http://www.imdb.com/search/title?groups=top_1000&sort=user_rating&view=simple'
-page = urllib.urlopen(url)
-soup = BeautifulSoup(page.read())
+pagesToVisit = range(1, 11)
+for eachpage in pagesToVisit:
+    url = 'movies/' + str(eachpage) + '.html'
+    page = urllib.urlopen(url)
+    soup = BeautifulSoup(page.read())
 
-artistName = soup.find_all('a', href=True)
-
-for artist in artistName:
-    artistList.append([unicode(artist.text).encode("utf-8")])
+    artistName = soup.findAll(href=re.compile('title'))
+    for artist in artistName:
+        artistList.append([unicode(artist.text).encode("utf-8")])
 
 musicFile = open('imdb1000movies.csv', 'w')
 writer = csv.writer(musicFile)
